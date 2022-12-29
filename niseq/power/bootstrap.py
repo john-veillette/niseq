a761dc9785c0eea057d0d9ad90fa7865b0fc5741
john-veillette <johnv@uchicago.edu>
2022-12-29 15:18:09 -0500

    expands util.py into a module and adds complete docstring to bootstrap_predictive_power_1samp

--- niseq/power/bootstrap.py
+++ niseq/power/bootstrap.py
@@ -42,7 +42,7 @@ def _boot_stat(x, statistic, n_resample, conditional = False, seed = 0, **kwargs
 def _bootstrap(x, statistic, n_resample, n_simulations,
                 conditional = False, seed = 0, n_jobs = 1, **kwargs):
     '''
-    performs Bayesian bootstrap, optionally conditioned on current data
+    performs full Bayesian bootstrap, optionally conditioned on current data
     '''
     # pick new random seeds to use for each permutation
     rng = check_random_state(seed)
@@ -74,15 +74,118 @@ def _expected_sample_size(rejs, look_times):
     return np.mean(ns)
 
 def bootstrap_predictive_power_1samp(X, test_func, look_times, n_max,
-                conditional = False, n_simulations = 1024, seed = 0,
+                conditional = False, n_simulations = 1024, seed = None,
                 n_jobs = 1, **test_func_kwargs):
-    '''
-    Computes the Bayesian predictive power non-parametrically using the Bayesian
+    '''Predictive power analysis via Bayesian bootstrap
+
+    Computes the predictive power non-parametrically using the Bayesian
     bootstrap. Optionally, you can condition on the current data to get
-    conditional power, which is useful for adaptive designs.
+    conditional power, which is useful for adaptive designs. Only valid for
+    one-sample (or paired-sample) tests.
+
+    Statistics computed from resamples using the Bayesian bootstrap, as opposed
+    to the frequentist boostrap, can be interpreted as draws from the posterior
+    distribution with an uninformative prior [1]. Thus, results here can be
+    conveniently interpreted as the Bayesian predictive power. As recommended by
+    [2] (not in English) and helpfully restated by [3] (in English), resampling
+    weights are drawn from Dirichlet(alpha = 4) for a better approximation.
+
+    This functionality is experimental. It is the best catch-all way to do a
+    power analysis for permutation tests I can think of, and similar resampling
+    approaches to estimating power have been used in the literature (e.g. by
+    [4]); however, it should be noted that the neuroimaging literature has not
+    converged upon a standardized approach to performing power analyses.
+    The Bayesian bootstrap approach used here incorporates uncertainty about
+    the effect size into the power estimate, which is handy since uncertainty
+    about the true effect size is considerable following a small pilot study, or
+    even a typical psychology/neuroimaging sample size, as pointed out by [5].
+
+    Parameters
+    -----------
+    X : list of array, shape (n_observations, p[, q][, r])
+        The data from which to resample. ``X`` should contain the observations
+        for one group or paired differences. The first dimension of each array
+        is the number of observations from that group; remaining dimensions
+        comprise the size of a single observation. See documentation for
+        user-input ``test_func`` for more details.
+    test_func : function
+        The one-sample sequential test you want to run a power analysis for.
+        Must accept ``look_times`` and ``n_max`` arguments and return results,
+        the middle two of which are the p-values for each look and the adjusted
+        alphas, respectively. This could be any user-facing function from
+        ``niseq`` that ends in ``_1samp``.
+    %(look_times)s
+    %(n_max)s
+    conditional : bool, default: ``False``
+        If ``True``, performs a conditional power analysis; that is, computes
+        the probability of a design rejecting the null hypothesis given that the
+        data in ``X`` has already been collected and is included in the
+        analysis, as in an adaptive design. If ``False`` (default), performs a
+        prospective power analysis (e.g. if you're using pilot data or data from
+        another study to inform sample size planning for a study that hasn't
+        begun data collection).
+    n_simulations: int, default: ``1024``
+        Number of bootstrap resamples/simulations to perform.
+    %(seed)s
+    %(n_jobs)s
+    **test_func_kwargs:
+        You may input any arguments you'd like passed to ``test_func``.
+
+
+    Returns
+    --------
+    res : dict
+        A results dictionary with keys:
+
+        uncorr_instantaneous_power : list of float
+            The power of a fixed-sample statistical test performed at each look.
+        rejection_probability : list of float
+            The probability that a sequential test rejects the null hypothesis
+            (for the first time) at each look time.
+        cumulative_power : list of float
+            The power of a sequential test to reject the null hypothesis by each
+            look time. ``res['cumulative_power'][-1]`` is the power of the full
+            sequential procedure.
+        uncorr_cumulative_power : list of float
+            Cumulative power if the rejection threshold at each look was not
+            corrected using alpha-spending (as it should be).
+        n_expected : float
+            The expected sample size for the sequential procedure.
+        n_simulations : int
+            The number of bootstrap resamples used.
+        n_orig_data : int
+            The sample size of the original data ``X``, i.e. ``X.shape[0]``.
+        conditional : bool
+            Whether the power analysis that was run was conditional (``True``)
+            or prospective (``False``).
+        test_func : str
+            Name of the sequential test function used.
+        test_func_kwargs : dict
+            A record of the arguments passed to the test function, including
+            ``look_times`` and ``n_max``.
+
+    Notes
+    --------
+    The significance level of the test used is specified in ``test_func``, and
+    thus can be modified by passing an argument to ``test_func`` using
+    ``**test_func_kwargs``.
+
+    References
+    ---------
+    .. [1] Rubin, D. B. (1981). The bayesian bootstrap.
+        The annals of statistics, 130-134.
+    .. [2] Tu, D. & Zheng, Z. (1987). The Edgeworth expansion for the random
+        weighting method. Chinese J. Appl. Probability and Statist., 3, 340-347.
+    .. [3] Shao, J., & Tu, D. (2012). The jackknife and bootstrap.
+        Springer Science & Business Media.
+    .. [4] Ruzzoli, M., Torralba, M., Fernández, L. M., & Soto-Faraco, S. (2019).
+        The relevance of alpha phase in human perception. Cortex, 120, 249-268.
+    .. [5] Lakens, D., & Evers, E. R. (2014). Sailing from the seas of chaos
+        into the corridor of stability: Practical recommendations to increase
+        the informational value of studies.
+        Perspectives on psychological science, 9(3), 278-292.
+
     '''
-    if 'y' in test_func_kwargs:
-        raise Exception('Independent-sample test input, one-sample expected!')
     def boot_stat(x, **kwargs): # thinly wrap test function
         _, ps, adj_alphas, _ = test_func(x, **kwargs)
         return np.stack([ps <= .05, ps <= adj_alphas], axis = 1)
@@ -95,7 +198,7 @@ def bootstrap_predictive_power_1samp(X, test_func, look_times, n_max,
     )
 
     # return simulation results with some metadata for posterity
-    results = {}
+    results = dict()
     results['uncorr_instantaneous_power'] = rejections[...,0].mean(0).tolist()
     results['rejection_probability'] = _prob_rejection(rejections[...,1]).tolist()
     results['cumulative_power'] = _cumulative_power(rejections[...,1]).tolist()
