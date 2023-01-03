66251be0cac53a08ad427377fb7140ee14823c57
John Veillette <johnv@uchicago.edu>
2023-01-03 10:27:48 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys 
+
+setup(
+    name = 'niseq',
+    version = '0.0.1',
+    description = 'Group sequential tests for neuroimaging',
+    url = 'https://github.com/john-veillette/mne-ari',
+    author = 'John Veillette',
+    author_email = 'johnv@uchicago.edu',
+    license = 'BSD-3-Clause',
+    packages = find_packages(),
+    install_requires = ['mne>=1.0'],
+    classifiers = [
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved',
+            'Programming Language :: Python',
+            'Topic :: Software Development',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Operating System :: MacOS',
+            'Programming Language :: Python :: 3',
+    ]
+)
